e68e42fb43d390248b9db8d9e7a5721e18b1bdc6
vishin10 <vishnuavula5@gmail.com>
2025-06-08 23:22:22 -0400

    nw firbase

--- backend/server/firebaseAdmin.ts
+++ backend/server/firebaseAdmin.ts
@@ -2,11 +2,21 @@ import { initializeApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getStorage } from 'firebase-admin/storage';
 import { ServiceAccount } from 'firebase-admin';
-import serviceAccount from './serviceAccountKey.json';
+
+const serviceAccountJSON = process.env.SERVICE_ACCOUNT_JSON_BASE64;
+
+if (!serviceAccountJSON) {
+  throw new Error("❌ Missing Firebase credentials in environment variable.");
+}
+
+const serviceAccount = JSON.parse(
+  Buffer.from(serviceAccountJSON, 'base64').toString('utf-8')
+) as ServiceAccount;
 
 const app = initializeApp({
-  credential: cert(serviceAccount as ServiceAccount),
-  storageBucket: "cardealer-york.firebasestorage.app",});
+  credential: cert(serviceAccount),
+  storageBucket: 'cardealer-york.firebasestorage.app',
+});
 
 export const db = getFirestore(app);
 export const bucket = getStorage().bucket();
